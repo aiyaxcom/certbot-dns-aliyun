1746eb90b459821085e67ef90036211e0a052b01
Jason Wang <954014787@qq.com>
2023-09-03 00:49:40 +0800

    update

--- configure-renew-cron.sh
+++ configure-renew-cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 设置要执行的命令
-command="docker run -it --rm --name certbot-dns-aliyun -v \"/etc/letsencrypt:/etc/letsencrypt\" -v \"/var/lib/letsencrypt:/var/lib/letsencrypt\" -e ALIYUN_CLI_ACCESS_KEY_ID=\"your-access-key-id\" -e ALIYUN_CLI_ACCESS_KEY_SECRET=\"your-access-key-secret\" aiyax/certbot-dns-aliyun renew --manual --preferred-challenges dns --manual-auth-hook 'aliyun-dns' --manual-cleanup-hook 'aliyun-dns clean' --non-interactive"
+command="docker run -it --rm --name certbot-dns-aliyun -v \"/etc/letsencrypt:/etc/letsencrypt\" -v \"/var/lib/letsencrypt:/var/lib/letsencrypt\" -e ALIYUN_CLI_ACCESS_KEY_ID=\"${ALIYUN_CLI_ACCESS_KEY_ID}\" -e ALIYUN_CLI_ACCESS_KEY_SECRET=\"${ALIYUN_CLI_ACCESS_KEY_SECRET}\" aiyax/certbot-dns-aliyun renew --manual --preferred-challenges dns --manual-auth-hook 'aliyun-dns' --manual-cleanup-hook 'aliyun-dns clean' --non-interactive"
 
 # 添加任务到crontab
 (crontab -l ; echo "0 3 5,15,25 * * $command") | crontab -
