522b00f154bc73e7c0949ade6fb17d17b1ee550c
Jason Wang <954014787@qq.com>
2023-09-02 22:52:36 +0800

    optimize

--- aliyun-dns.sh
+++ aliyun-dns.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-configure-aliyun-cli
+echo 'Start configuring Aliyun CLI...'
+
+aliyun configure set \
+  --profile akProfile \
+  --mode AK \
+  --region cn-qingdao \
+  --access-key-id $ALIYUN_CLI_ACCESS_KEY_ID \
+  --access-key-secret $ALIYUN_CLI_ACCESS_KEY_SECRET
+
+echo 'Completed Aliyun CLI configuration!'
 
 DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
 SUB_DOMAIN=$(expr match "$CERTBOT_DOMAIN" '\(.*\)\..*\..*')
